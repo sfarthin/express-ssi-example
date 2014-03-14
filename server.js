03ea96a72d34a6cc20dbd1b851c268577941f074
Steven Farthing <sfarthin@gmail.com>
2014-03-14 16:48:32 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express"),
+	ssi		= require("ssi"),
+	path 	= require("path"),
+	fs 		= require("fs"),
+	app 	= express(),
+	
+	parser 		= new ssi(__dirname, "", "");
+	
+// Handle server side includes for html files
+app.use(function(req,res,next) {
+	var filename 	= __dirname+(req.path == "/" ? "/index" : req.path);
+
+	if(fs.existsSync(filename)) {
+		res.send(parser.parse(filename, fs.readFileSync(filename, {encoding: "utf8"})).contents);	
+	} else {
+		next();
+	}
+});
+
+
+app.listen(process.env.PORT || 3001)
