7549eb9bbc06eee020b0994cccd31a6abd237eea
Steven Farthing <sfarthin@gmail.com>
2014-03-14 16:55:26 -0400

    small fix for index files

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require("express"),
 	
 // Handle server side includes for html files
 app.use(function(req,res,next) {
-	var filename 	= __dirname+(req.path == "/" ? "/index" : req.path);
+	var filename 	= __dirname+(req.path == "/" ? "/index.shtml" : req.path);
 
 	if(fs.existsSync(filename)) {
 		res.send(parser.parse(filename, fs.readFileSync(filename, {encoding: "utf8"})).contents);	
